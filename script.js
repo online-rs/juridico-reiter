f4224a938e480d04619bb5178a662ee55a5b12fa
online-rs <brunorstkd@gmail.com>
2022-11-11 09:31:57 -0300

    com dadas

--- script.js
+++ script.js
@@ -33,3 +33,41 @@ function conversar(){
   }
 
 }
+
+function abrirConvite(){
+  var abaConvite = document.getElementById('convite');
+  var status = abaConvite.style.display;
+  var satusblock = abaConvite.style.display = 'block';
+  var satusnone = abaConvite.style.display = 'none';
+  console.log(status);
+  if (status === satusnone){
+    console.log('status estava none');
+    document.getElementById('convite').style.display = 'block';
+  }else{
+    console.log('status estava block');
+    document.getElementById('convite').style.display = 'none';
+  }
+}
+  
+
+function convidarTestemunha(){
+  var numeroWhatsapp = document.getElementById('numeroConvite').value;
+  var numeroddd = document.getElementById('dddConvite').value;
+  var nome = document.getElementById('nome').value;
+  var reclamante = document.getElementById('reclamante').value;
+  var processo = document.getElementById('processo').value;
+  var data = document.getElementById('data').value;
+  var horario = document.getElementById('horario').value;
+  var modalidade = document.getElementById('modalidade').value;
+  var local = document.getElementById('local').value;
+  var link = document.getElementById('link').value;
+  if (numeroWhatsapp == ''){
+      alert('Favor colocar um número')
+  }else{
+      var caminho = 'https://wa.me/55' + numeroddd + numeroWhatsapp +'?text=Oi ' + nome + ', tudo bem?%0A%0ATeremos uma audiência de instrução do ex-funcionário '+ reclamante + '(' + processo + ')%0A%0AData: ' + data +'%0AHorário: ' + horario + '%0AModalidade: ' + modalidade + '%0ALocal: ' + local + '%0ALink: ' + link + '%0A%0A Poderia participar como testemunha, por favor? %0A%0AAbaixo está o link para instalação do aplicativo ZOOM. Favor aceitar todas as permissões, principalemnte áudio e video, para que não haja problemas na conexão. %0A%0ALink de instalação do ZOOM: https://play.google.com/store/apps/details?id=us.zoom.videomeetings&hl=pt_BR&gl=US  ' 
+      window.open(caminho);
+      document.getElementById('site').textContent = caminho;
+      document.getElementById('site').style.display = 'inline';
+  }
+
+}
